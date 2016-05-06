d00a3e33d619e370127c0aaf68731154ebd19961
Uwe Loydl <uloydl@team.mobile.de>
2016-05-06 11:46:46 +0200

    added base selenium node

--- nodes/firefox/generate.sh
+++ nodes/firefox/generate.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 VERSION=$1
 
+cp ../base/entry.sh entry.sh
+cp ../base/functions.sh functions.sh
+
 echo FROM uweloydl/robot-base:$VERSION > ./Dockerfile
-cat ../Dockerfile.node.txt >> ./Dockerfile
+cat ../base/Dockerfile.node.txt >> ./Dockerfile
 cat ./Dockerfile.firefox.txt >> ./Dockerfile
